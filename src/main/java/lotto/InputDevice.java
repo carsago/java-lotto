1a1ed29a4fb731d8a20642c25c7172faf00e630f
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-09 17:02:14 +0900

    feat: 금액을 입력하여 로또를 구매하는 기능
    InputDevice class 생성
    InputDevice.setMoneyToBuyLotto() method 추가

---
+++ src/main/java/lotto/InputDevice.java
@@ -0,0 +1,11 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputDevice {
+
+    public String setMoneyToBuyLotto() {
+        String input = Console.readLine();
+        return input;
+    }
+}
